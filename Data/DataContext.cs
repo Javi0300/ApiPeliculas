859ee2cc437077a3651486a3baa5ded609578127
Javi0300 <93288606+Javi0300@users.noreply.github.com>
2022-01-25 14:04:41 -0600

    Add files via upload

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApiPeliculas.Modelos;
+
+namespace ApiPeliculas.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Peliculas> Peliculas { get; set; }
+    }
+}
---
+++ Modelos/Peliculas.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos
+{
+    public class Peliculas
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Titulo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Director { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Genero { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public int Rating { get; set; }
+
+        [Required]
+        public DateTime AñoPublicacion { get; set; }
+    }
+}
